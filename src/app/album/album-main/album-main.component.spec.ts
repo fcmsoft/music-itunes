d3427c0c71ac2f6a2cec9f6f32b1c9dbf9d887e5
Flavia Moyano <flavia.moyano@synapse.com>
2019-02-10 17:25:44 -0300

    minnor change to test travis

--- src/app/album/album-main/album-main.component.spec.ts
+++ src/app/album/album-main/album-main.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AlbumMainComponent } from './album-main.component';
 import { AlbumRoutingModule } from '../album-routing.module';
 
+
 describe('AlbumMainComponent', () => {
   let component: AlbumMainComponent;
   let fixture: ComponentFixture<AlbumMainComponent>;
